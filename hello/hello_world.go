9ccf4ab3dcd68315c9cf4e195c27bb27ad896b52
Helena Converse <helena.converse@asynchrony.com>
2017-09-28 12:56:30 -0500

    hello

---
+++ hello/hello_world.go
@@ -0,0 +1,11 @@
+package main
+  /* every source code must declare a package
+   * main is a special package.
+   * Must live with "func main"
+   */
+
+import "fmt" //fmt is formatted input-output
+
+func main() { //main, standard fair for a "main" function.
+  fmt.Println("Hello, World!")
+}
