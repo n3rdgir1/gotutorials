ef4a938ff02bf0a5baf409ec34988abccbd1ec62
Helena Converse <helena.converse@asynchrony.com>
2017-09-28 13:00:18 -0500

    import package notes

--- hello/hello_world.go
+++ hello/hello_world.go
@@ -9,3 +9,12 @@ import "fmt" //fmt is formatted input-output
 func main() { //main, standard fair for a "main" function.
   fmt.Println("Hello, World!")
 }
+
+/*
+pwd: ~/go/src/foo, import_path: foo, default_name: foo
+pwd: ~/go/src/foo/bar, import_path: foo/bar, default_name: bar
+pwd: ~/go/src/foo/bar/bazz, import_path: foo/bar/bazz, default_name: bazz
+
+override default name:
+  import ba "foo/bar/bazz" => now it's ba
+*/
